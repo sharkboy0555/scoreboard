0f7521477c556dd4f178165bdc02c71cf449367b
sharkboy0555 <zchambers@zimsbagging.com>
2025-07-15 16:20:12 -0400

    Update fpp_install.sh

--- scoreboard-plugin/scripts/fpp_install.sh
+++ scoreboard-plugin/scripts/fpp_install.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# This script runs after the plugin is cloned
-
-echo "Running Scoreboard plugin install script..."
-
-# You can add setup commands here if needed
-
+echo "Installing Scoreboard Plugin..."
+chmod +x *.php
 exit 0
