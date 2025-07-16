3b4bb1004f6c68db90c7fb43206ff5795db29c18
sharkboy0555 <zchambers@zimsbagging.com>
2025-07-16 08:43:15 -0400

    Create status.php

---
+++ status.php
@@ -0,0 +1,17 @@
+<?php
+include_once "../../common.php";
+include_once "../../functions.php";
+include_once "../../plugin.php";
+
+$pluginName = "scoreboard-plugin";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Scoreboard Plugin Status</title>
+</head>
+<body>
+    <h1>Scoreboard Plugin</h1>
+    <p>This is the status page. Plugin loaded successfully.</p>
+</body>
+</html>
