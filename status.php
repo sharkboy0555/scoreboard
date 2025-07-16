38284f5d8ecc79c1df822a9224c531e064355ae0
sharkboy0555 <zchambers@zimsbagging.com>
2025-07-16 14:44:22 -0400

    Update status.php

--- status.php
+++ status.php
@@ -1,15 +1,7 @@
 <?php
-// Load settings
-$settingsFile = '/home/fpp/media/plugins/scoreboard-plugin/settings.json';
-$settings = [];
-
-if (file_exists($settingsFile)) {
-    $settings = json_decode(file_get_contents($settingsFile), true);
-}
-
 header('Content-Type: application/json');
 echo json_encode([
     'status' => 'ok',
-    'settings' => $settings
+    'message' => 'Scoreboard plugin loaded successfully'
 ]);
 ?>
