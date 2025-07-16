f8c5905fb067190be46821f247469e2c0c7016e2
sharkboy0555 <zchambers@zimsbagging.com>
2025-07-16 15:03:04 -0400

    Update ScoreboardOverlay.pm

--- ScoreboardOverlay.pm
+++ ScoreboardOverlay.pm
@@ -1,3 +1,17 @@
+package ScoreboardOverlay;
+
+use strict;
+use warnings;
+use base 'PixelOverlayModel';
+use JSON;
+
+sub new {
+    my ($class, $name, $channel, $startChannel, $width, $height, $args) = @_;
+    my $self = $class->SUPER::new($name, $channel, $startChannel, $width, $height, $args);
+    bless($self, $class);
+    return $self;
+}
+
 sub renderFrame {
     my ($self, $frameRef, $frameNum) = @_;
     $self->ClearFrame($frameRef);
@@ -5,25 +19,15 @@ sub renderFrame {
     my $settingsFile = "/home/fpp/media/plugins/scoreboard/settings.json";
     return unless -e $settingsFile;
 
-    my $json = do {
-        open my $fh, '<', $settingsFile or return;
-        local $/;
-        <$fh>;
-    };
-    my $settings = eval { JSON::decode_json($json) };
+    open my $fh, '<', $settingsFile or return;
+    local $/;
+    my $json_text = <$fh>;
+    close $fh;
+
+    my $settings = eval { decode_json($json_text) };
     return unless $settings;
 
+    # Prepare scoreboard text lines
     my @lines = (
-        "M: $settings->{machine}  O: $settings->{order}",
-        "P: $settings->{part}",
-        "Setup: $settings->{setup}/$settings->{goal}",
-        "Qty: $settings->{currentQty}/$settings->{orderQty}",
-        "BPH: $settings->{bphShift}/$settings->{bphStandard}"
-    );
-
-    my $y = 0;
-    for my $line (@lines) {
-        $self->DrawText($frameRef, 0, $y, $line, 0, 255, 0);
-        $y += 12;
-    }
-}
+        "M:$settings->{machine} O:$settings->{order}",
+        "P:$settings->
