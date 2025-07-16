2ff056abdcf5f352ffffad6846ef3d7a8469a747
sharkboy0555 <zchambers@zimsbagging.com>
2025-07-16 14:53:07 -0400

    Update save.php

--- www/save.php
+++ www/save.php
@@ -1,21 +1,20 @@
 <?php
-$settingsFile = '/home/fpp/media/plugins/scoreboard-plugin/settings.json';
+$settingsFile = '/home/fpp/media/plugins/scoreboard/settings.json';
 
 $settings = [
     'machine' => $_POST['machine'] ?? '',
     'order' => $_POST['order'] ?? '',
     'part' => $_POST['part'] ?? '',
+    'setup' => $_POST['setup'] ?? '',
+    'goal' => $_POST['goal'] ?? '',
     'currentQty' => $_POST['currentQty'] ?? '',
     'orderQty' => $_POST['orderQty'] ?? '',
-    'setupTime' => $_POST['setupTime'] ?? '',
-    'setupGoal' => $_POST['setupGoal'] ?? ''
+    'bphShift' => $_POST['bphShift'] ?? '',
+    'bphStandard' => $_POST['bphStandard'] ?? ''
 ];
 
 file_put_contents($settingsFile, json_encode($settings));
 
-// Optional: Send to matrix
-$msg = "Machine: {$settings['machine']} | Order: {$settings['order']} | Qty: {$settings['currentQty']}/{$settings['orderQty']}";
-exec("/home/fpp/media/plugins/scoreboard-plugin/scripts/send_to_matrix.sh " . escapeshellarg($msg));
-
+// Optionally trigger matrix redraw here (or handled via polling/renderFrame)
 echo json_encode(['status' => 'saved']);
 ?>
